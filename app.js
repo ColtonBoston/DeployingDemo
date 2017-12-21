033e156bcb04a5ae85639b8250d493131f36ff1f
Colton Boston <coltonbboston@gmail.com>
2017-12-21 12:30:05 -0600

    fix error is app.listen

--- app.js
+++ app.js
@@ -11,4 +11,4 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(process.env.PORT< process.env.IP, console.log("listening..."));
+app.listen(process.env.PORT, process.env.IP, console.log("listening..."));

11ddaebaecc310ec300070b636bd3d192a000170
Colton Boston <coltonbboston@gmail.com>
2017-12-21 12:28:45 -0600

    change port 3000 to process.env.POST and process.env.IP

--- app.js
+++ app.js
@@ -11,4 +11,4 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen("3000", console.log("listening..."));
+app.listen(process.env.PORT< process.env.IP, console.log("listening..."));
